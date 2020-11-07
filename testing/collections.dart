d003b021edc45455043f8d72e15c58dc93aa7c73
Nikolai Madridano <nikol_m18@yahoo.com>
2020-11-07 12:10:31 +0800

    init data-structures

---
+++ testing/collections.dart
@@ -0,0 +1,41 @@
+//[Person class]
+class Person {
+  String name;
+  Person(this.name);
+}
+
+main(List<String> args) {
+  //Two ways of creating a list
+  List<String> girls = [];
+  List boys = new List<String>();
+
+  //Adding a value to a list
+  girls.add("Ryujin");
+  boys.add("JYP");
+
+  //List using a class
+  var person1 = new Person("Seo Ye-ji");
+  var person2 = new Person("Ryujin");
+  var person3 = new Person("Lia");
+  var person4 = new Person("Yeji");
+  var person5 = new Person("Yuna");
+  var person6 = new Person("Chaeryoung");
+
+  //Create a new list
+  var listOfPerson = new List<Person>();
+  //Add
+  listOfPerson.add(person1);
+  listOfPerson.add(person2);
+  listOfPerson.add(person3);
+  listOfPerson.add(person4);
+  listOfPerson.add(person5);
+  listOfPerson.add(person6);
+
+  //Loop through a list
+  for (var person in listOfPerson) {
+    print(person.name);
+  }
+
+  //print(girls);
+  //print(boys);
+}
---
+++ testing/map.dart
@@ -0,0 +1,12 @@
+main(List<String> args) {
+  //Map
+  var winners = {
+    // key & value
+    "first": "Lia",
+    "second": "Seea",
+    "third": "Ryujin"
+  };
+  winners.forEach((key, value) {
+    print({key, value});
+  });
+}
